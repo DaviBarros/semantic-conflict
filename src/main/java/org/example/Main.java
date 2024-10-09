d9b824d6a645da054f15747c3c84ac59cc39716b
Davi de Barros <davibarros@hotmail.com>
2024-10-09 15:49:59 -0300

    arquivos alterados 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 import samples.*;
 
 public class Main {
+    int x = 0;
+
     public static void main(String[] args) {
         OverridingAssignmentLocalVariablesSample.conflict();
+        x = 1;
     }
 }
--- src/main/java/samples/OverridingAssignmentLocalVariablesSample.java
+++ src/main/java/samples/OverridingAssignmentLocalVariablesSample.java
@@ -4,7 +4,7 @@ public class OverridingAssignmentLocalVariablesSample {
     public static int x, y;
     public static void conflict() {
         y = 2;   // left
-        x = x+1; // base
+        x = x+1+2; // base
         System.out.println(x);
     }
 }
